d17097c2c2be4adceeb4767f33483122ea85f86b
wazlid <wazlid@users.noreply.github.com>
2015-05-24 23:36:34 +0000

    Create text_expr.h

---
+++ text_expr.h
@@ -0,0 +1,27 @@
+/* 
+   Copyright (C) 2014.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+/* Written by Walid Chokki <wazlid@programmerenc.com>.  */
+#ifndef __TEST_EXPRESSION__
+	#define __TEST_EXPRESSION__
+	#include "stack.h"
+	int 
+	test_char(char *expression);
+	int 
+	delete_space(char *expression);
+	int 
+	test_expression(char *expression);
+#endif
